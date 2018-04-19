ebfde9491932c7bed9404dc162baa06b9b062dd8
failgracefully <36736698+failgracefully@users.noreply.github.com>
2018-04-19 15:14:07 +0200

    Enabled custom workingdir run while calling hugo (#22)

--- scripts/generate-data-branch.sh
+++ scripts/generate-data-branch.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 #
 # Execute this script in the repository root.
+WORKDIR=$1
 TOOLS_DIR="$PWD/tools/databranches"
-DATA_DIR=`$TOOLS_DIR/create-with-hugo.sh`
+DATA_DIR=`$TOOLS_DIR/create-with-hugo.sh $WORKDIR`
 cp CNAME $DATA_DIR/
 $TOOLS_DIR/create-data-branch.sh $DATA_DIR/ gh-pages
